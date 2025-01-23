2a3299574d8497edac802e6f939b879e6d9025b4
Christopher Lozinski <34622952+PythonLinks@users.noreply.github.com>
2025-01-23 14:06:23 +0100

    Update copyright in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,18 @@
 # -- Project information
 
 project = 'Mecrisp Ice'
-copyright = '2024, Christopher Lozinski and Matthias Koch'
+copyright = """ `Mecrisp Ice
+Documentation <https://mecrisp-ice.readthedocs.io>`__
+by Christopher Lozinski and Matthias Koch is licensed under `CC BY-SA
+4.0\ |image1|\ |image2|\ |image3| <https://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1>`__
+
+.. |image1| image:: https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1
+.. |image2| image:: https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1
+.. |image3| image:: https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1
+
+"""
+
+
 author = 'Chrisopher Lozinski'
 
 release = '0.1'

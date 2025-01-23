6b92b676106fb679d17ed3fb6f16476a037cc645
Christopher Lozinski <34622952+PythonLinks@users.noreply.github.com>
2025-01-23 14:19:15 +0100

    Update conf.py copyright to mention license.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,11 +3,7 @@
 # -- Project information
 
 project = 'Mecrisp Ice'
-copyright = """ <p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><a property="dct:title" rel="cc:attributionURL" href="https://mecrisp-ice.readthedocs.io/en/latest/index.html">Mecrisp Ice Documentation</a> by <span property="cc:attributionName">Christopher Lozinski and Matthias Koch</span> is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY-SA 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1" alt=""></a></p>   
-
-"""
-
-
+copyright = '2024, Christopher Lozinski and Matthias Koch.  Licensed under CC BY-SA 4.'
 author = 'Chrisopher Lozinski'
 
 release = '0.1'
